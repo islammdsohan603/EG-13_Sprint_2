62c8a533135de7b1e4df4e43a75097785a0f14a3
islammdsohan603 <islammdsohan603@gmail.com>
2026-08-28 12:15:42 +0600

    Find Maximum Problam solve

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -1,10 +1,15 @@
 
-// Reverse a String
+// Find Maximum
 
-function reverseString(str) {
+function findMax(nums) {
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > max) {
+      max = nums[i]
+    }
+  }
 
-  return str.split("").reverse().join("");
-
+  return max
 }
 
-console.log(reverseString("hello"))
+console.log(findMax([2, 7, 2, 9, 5]))
