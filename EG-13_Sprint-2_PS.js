48f1a668ceee6bfc95a2b39fd8b6ac03672f4efc
islammdsohan603 <islammdsohan603@gmail.com>
2026-08-28 17:15:19 +0600

    Two Sum

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -1,17 +1,14 @@
 
+// Two Sum
 
-//Count Vowels
-
-function countVowels(str) {
-
-  let count = 0;
-  const vowels = 'aeiou';
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
-      count++
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
   }
-  return count
 }
 
-console.log(countVowels("javascript"))
+console.log(twoSum([2, 7, 11, 15], 9))
